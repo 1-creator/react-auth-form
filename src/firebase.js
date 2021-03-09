404efe666c3fdbdeb21038510eceb20397c46966
valentineG <61587290+1-creator@users.noreply.github.com>
2021-03-09 13:22:15 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,9 @@ import firebase from 'firebase';
 import * as firebaseui from 'firebaseui'
 
 
-//firebase config info
+//your firebase config info
 var firebaseConfig = {
-    apiKey: "AIzaSyCJJKZQzI_JumBZzHcy0mrCEja9e7fVJVw",
-    authDomain: "authform-4d7d6.firebaseapp.com",
-    projectId: "authform-4d7d6",
-    storageBucket: "authform-4d7d6.appspot.com",
-    messagingSenderId: "587709552247",
-    appId: "1:587709552247:web:e1facee08fa13d2cf95872"
+    
 };
 
 
